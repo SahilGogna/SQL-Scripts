74334b3e2c38989c714be9faf8313674c1ccfcbf
Sahil Gogna <gogna.sahil95@gmail.com>
2022-03-14 19:20:08 -0300

    Session 3

--- Techtfq-online sessions/Session 2/My Scripts/Assignment1.sql
+++ Techtfq-online sessions/Session 2/My Scripts/Assignment1.sql
@@ -4,3 +4,18 @@ from teams home
 cross join teams away
 where home.team_code != away.team_code
 order by home.team_name desc;
+
+/*
+This is a very poor method. Cross joins should only be preferred when the 
+result generates only one value, or one of the table has only one value.
+We will use inner join.
+*/
+
+
+select home.team_name, away.team_name 
+from teams home
+inner join teams away
+on home.team_code != away.team_code
+order by home.team_name desc;
+
+
